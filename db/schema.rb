e8c9c7d2db3b00920b6952976c70253d3ac1e282
jlaurendi <jlaurendi@gmail.com>
2024-08-15 21:37:48 -0700

    Add devise for User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_16_043116) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_16_043614) do
   create_table "marathons", force: :cascade do |t|
     t.string "name"
     t.string "city"
@@ -27,5 +27,17 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_16_043116) do
     t.string "abbreviation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 end
