f8538493b88e16a01d1db03ffed247aa1c219df5
jlaurendi <jlaurendi@gmail.com>
2024-08-25 09:38:12 -0700

    Add in eslint / prettier

--- frontend/src/components/UserGoalModalComponent.tsx
+++ frontend/src/components/UserGoalModalComponent.tsx
@@ -1,13 +1,15 @@
-import React from "react"
+import React, { useState } from "react"
 import "./UserGoalModalComponent.css"
 
 const UserGoalModalComponent = ({ isOpen, onClose, selectedState }) => {
+    const [mode] = useState('empty') 
+
     if (!isOpen) return null
 
     return (
         <div className="modal-overlay">
             <div className="modal-content">
-                <h2 className="modal-title">{selectedState} Goal</h2>
+                <h2 className="modal-title">{selectedState} Marathon</h2>
                 <p>Information about state to go here</p>
                 <button onClick={onClose}>Close</button>
             </div>
